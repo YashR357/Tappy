d26a8a125080e117275644b5b7d750be1ad5001d
patrick-chen8 <72107137+patrick-chen8@users.noreply.github.com>
2022-02-19 16:41:08 -0500

    Homepage skeleton
    created homepage and logo, next step is to create logo component

--- tappy/src/app/app.module.ts
+++ tappy/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StyleComponent } from './style/style.component';
 import { CartComponent } from './cart/cart.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     ShopComponent,
     StyleComponent,
     CartComponent,
-    NavBarComponent
+    NavBarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
