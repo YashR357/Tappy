bfa09b4229ba4b6ab7b849fff248550917e9686f
Yash Roygaga <yashroygaga@gmail.com>
2022-02-19 20:29:16 -0500

    new proj

--- tappy/src/app/app-routing.module.ts
+++ tappy/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { CartComponent } from './cart/cart.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ShopComponent } from './shop/shop.component';
-import { StyleComponent } from './style/style.component';
+import { ServiceSelectComponent } from './service-select/service-select.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: HomepageComponent },
-  { path: 'Shop', component: ShopComponent },
-  { path: 'AboutUs', component: AboutUsComponent },
-  {path: 'Style', component: StyleComponent}
+  { path: 'ServiceSelect', component: ServiceSelectComponent },
+  
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
